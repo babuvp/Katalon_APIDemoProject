4c64ebce2461b2ec6074938df43ef27d39a15d44
babu_ <babu_@DESKTOP-8F21D57>
2021-02-07 07:59:05 -0600

    First Project Commit

---
+++ Test Suites/NewTestSite1.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>NewTestSite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>1d8f2ba7-4045-42e4-b2c3-4ec79eb19ca8</testSuiteGuid>
+   <testCaseLink>
+      <guid>6de47233-a84b-4249-a3d4-b8c6cb68d8e9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GetBankDetails</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c6c5a0d6-a102-492b-9626-07fbd16d4b36</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ListUsersTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c4009644-6f83-4100-92cb-11f3914f9990</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestHttpMethods</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e7168344-24db-42db-955e-3caad3cd44e3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CalculatorTests/Addition</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
